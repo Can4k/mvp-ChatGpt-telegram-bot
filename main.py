b023c80705f709137a23665afe3de8fe68842258
Can4k <yakovleva24@sch57.ru>
2023-05-10 14:27:28 +0300

    Добавление комментариев

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from assets.config import API
 from utils import find_match, count_files_in_folder
 from assets.text_scheme import scheme
 
+# количество фоток кошек
 COUNT_OF_CATS = count_files_in_folder('assets/cats')
 
 
+# обработчик текста
 async def get_answer(update, context):
     text = update.message.text
     for ITEM in scheme.keys():
@@ -18,17 +20,20 @@ async def get_answer(update, context):
     await update.message.reply_text(scheme['DEFAULT']['response'])
 
 
+# обработчик отправки кошки
 async def cat_command(update, context):
     await context.bot.send_photo(chat_id=update.effective_chat.id,
                                  photo=open(f'assets/cats/cat{randint(0, COUNT_OF_CATS - 1)}.png', 'rb'))
 
 
+# обработчик /help
 async def help_command(update, context):
     await update.message.reply_text(
         'Я понимаю команды <a>/help</a> и <a>/cat</a>, могу оценить фотокарточку,'
         ' а также очень доброжелательный!', parse_mode='HTML')
 
 
+# обработчик получения фотографии
 async def image_taking(update, context):
     await update.message.reply_text('Отличная фотокарточка!')
 
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
 
 
+# считаем количество файлов в папке
 def count_files_in_folder(folder_path):
     files = os.listdir(folder_path)
     return len([f for f in files if os.path.isfile(os.path.join(folder_path, f))])
 
 
+# проверям наличие слов из pattern в text
 def find_match(text, pattern):
-    return any([word in text.lower() for word in pattern])
+    return any([word.lower() in text.lower() for word in pattern])
